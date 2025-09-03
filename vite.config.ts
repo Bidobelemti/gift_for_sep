a0b1b191bb1266bb5ad7eb05c347f4241551839d
bidobelemti <mauriceuve3221@gmail.com>
2025-09-02 22:28:34 -0500

    configuración para deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base : '/gift_for_sep/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
